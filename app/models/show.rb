d52a15b8d4b063b28fc97fa27396a1b31f883efd
Trevor Seitz <trevinla@gmail.com>
2018-04-22 22:14:15 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where("rating = maximum").orderrdsc).first.name
+    Show.where("rating = maximum").order(rating: :dsc).first.name
   end
 
 end
